8fa7ad46c84bf440eb959e4ac886152a70d644dd
Dominic Stettler <dominic.stettler@sunrise.ch>
2020-01-15 22:08:16 +0100

    including crud operations + extending list according to concept

--- HomeApp/HomeApp/ClientApp/src/app/components/task-item/task-item.component.ts
+++ HomeApp/HomeApp/ClientApp/src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-task-item',
@@ -6,8 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-item.component.css']
 })
 export class TaskItemComponent implements OnInit {
+  taskItems: any[];
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    http.get(this.url)
+      .subscribe(response => {
+        this.taskItems = response;
+      })
+  }
+
+  createTaskItem(input: HTMLInputElement) {
+    let taskItem = { title: input.value };
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(taskItem))
+      .subscribe(response => {
+        taskItem['id'] = response;
+        this.taskItems.splice(0, 0, taskItem);
+      })
+  }
+
+  updateTaskItem(taskItem) {
+    this.http.patch(this.url + '/' + taskItem.id, JSON.stringify({ isRead: true }))
+      //this.http.put(this.url + '/' + taskItem.id, JSON.stringify(taskItem))
+      .subscribe(response => {
+        console.log(response);
+      })
+  }
+
+  deleteTaskItem(taskItem) {
+    this.http.delete(this.url + '/' + taskItem.id)
+      .subscribe(response => {
+        let index = this.taskItems.indexOf(taskItem);
+        this.taskItems.splice(index, 1);
+      })
+  }
 
   ngOnInit() {
   }
