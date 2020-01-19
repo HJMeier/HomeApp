4271604cab050b608d766859dd5b557baf2941a1
Dominic Stettler <dominic.stettler@sunrise.ch>
2020-01-19 21:03:52 +0100

    set up of Backend using database and api, tested with postman

---
+++ HomeApp/HomeApp/Models/TaskItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeApp.Models
+{
+    public class TaskItem
+        //any changes to data model -> Packet Manager Console: Add-Migration YourMigration Name and Update-Database
+    {
+        [Key]
+        public int TaskId { get; set; }
+
+        [Required]
+        public string TaskTitle { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        public DateTime DoneDate { get; set; }
+
+        [Required]
+        public int State { get; set; }
+
+        [Required]
+        public int SeriesType { get; set; }
+    }
+}
--- HomeApp/HomeApp/Startup.cs
+++ HomeApp/HomeApp/Startup.cs
@@ -1,10 +1,15 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using HomeApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using HomeApp.Data;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace HomeApp
 {
@@ -18,10 +23,38 @@ namespace HomeApp
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        /* public void ConfigureServices(IServiceCollection services)
+         {
+             services.AddControllersWithViews();
+             // In production, the Angular files will be served from this directory
+             services.AddSpaStaticFiles(configuration =>
+             {
+                 configuration.RootPath = "ClientApp/dist";
+             });
+
+             services.AddDbContext<TaskItemContext>(options =>
+                     options.UseSqlServer(Configuration.GetConnectionString("TaskItemContext")));
+         }*/
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-            // In production, the Angular files will be served from this directory
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<TaskItemContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("TaskItemContext")));
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+
+            services.AddScoped(typeof(IDataRepository<>), typeof(DataRepository<>));
+
+            //In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
@@ -29,7 +62,7 @@ namespace HomeApp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -37,25 +70,20 @@ namespace HomeApp
             }
             else
             {
-                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
+            app.UseCors("CorsPolicy");
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            if (!env.IsDevelopment())
-            {
-                app.UseSpaStaticFiles();
-            }
+            //app.UseSpaStaticFiles();
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(routes =>
             {
-                endpoints.MapControllerRoute(
+                routes.MapRoute(
                     name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
+                    template: "{controller}/{action=Index}/{id?}");
             });
 
             app.UseSpa(spa =>
